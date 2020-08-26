c42a5a31394ccd66b748711813c143d8a7ab4d73
Xiao <xiao@scp-079.org>
2020-08-26 06:30:09 +0000

    channel.py 的重构

--- plugins/functions/channel.py
+++ plugins/functions/channel.py
@@ -23,6 +23,7 @@ from typing import List, Union
 from pyrogram import Chat, Client
 
 from .. import glovar
+from .decorators import threaded
 from .etc import code, code_block, general_link, lang, thread
 from .file import crypt_file, data_to_file, delete_file, get_new_path
 from .telegram import get_group_info, send_document, send_message
@@ -33,7 +34,10 @@ logger = logging.getLogger(__name__)
 
 def exchange_to_hide(client: Client) -> bool:
     # Let other bots exchange data in the hide channel instead
+    result = False
+
     try:
+        # Transfer the channel
         glovar.should_hide = True
         share_data(
             client=client,
@@ -49,17 +53,18 @@ def exchange_to_hide(client: Client) -> bool:
                 f"{lang('auto_fix')}{lang('colon')}{code(lang('protocol_1'))}\n")
         thread(send_message, (client, glovar.critical_channel_id, text))
 
-        return True
+        result = True
     except Exception as e:
         logger.warning(f"Exchange to hide error: {e}", exc_info=True)
 
-    return False
+    return result
 
 
 def format_data(sender: str, receivers: List[str], action: str, action_type: str,
                 data: Union[bool, dict, int, str] = None) -> str:
     # Get exchange string
-    text = ""
+    result = ""
+
     try:
         data = {
             "from": sender,
@@ -68,49 +73,58 @@ def format_data(sender: str, receivers: List[str], action: str, action_type: str
             "type": action_type,
             "data": data
         }
-        text = code_block(dumps(data, indent=4))
+        result = code_block(dumps(data, indent=4))
     except Exception as e:
         logger.warning(f"Format data error: {e}", exc_info=True)
 
-    return text
+    return result
 
 
 def get_debug_text(client: Client, context: Union[int, Chat, List[int]]) -> str:
     # Get a debug message text prefix
-    text = ""
+    result = ""
+
     try:
         # Prefix
-        text = f"{lang('project')}{lang('colon')}{general_link(glovar.project_name, glovar.project_link)}\n"
+        result = f"{lang('project')}{lang('colon')}{general_link(glovar.project_name, glovar.project_link)}\n"
 
         # List of group ids
-        if isinstance(context, list):
-            for group_id in context:
-                group_name, group_link = get_group_info(client, group_id)
-                text += (f"{lang('group_name')}{lang('colon')}{general_link(group_name, group_link)}\n"
-                         f"{lang('group_id')}{lang('colon')}{code(group_id)}\n")
+        gids = context if isinstance(context, list) else []
+
+        for group_id in gids:
+            group_name, group_link = get_group_info(client, group_id)
+            result += (f"{lang('group_name')}{lang('colon')}{general_link(group_name, group_link)}\n"
+                       f"{lang('group_id')}{lang('colon')}{code(group_id)}\n")
+
+        if gids:
+            return result
 
         # One group
+        if isinstance(context, int):
+            group_id = context
         else:
-            # Get group id
-            if isinstance(context, int):
-                group_id = context
-            else:
-                group_id = context.id
+            group_id = context.id
 
-            # Generate the group info text
-            group_name, group_link = get_group_info(client, context)
-            text += (f"{lang('group_name')}{lang('colon')}{general_link(group_name, group_link)}\n"
-                     f"{lang('group_id')}{lang('colon')}{code(group_id)}\n")
+        # Generate the group info text
+        group_name, group_link = get_group_info(client, context)
+        result += (f"{lang('group_name')}{lang('colon')}{general_link(group_name, group_link)}\n"
+                   f"{lang('group_id')}{lang('colon')}{code(group_id)}\n")
     except Exception as e:
         logger.warning(f"Get debug text error: {e}", exc_info=True)
 
-    return text
+    return result
 
 
-def send_debug(client: Client, chat: Chat, action: str, aid: int, config_type: str = "",
-               more: str = "") -> bool:
+@threaded()
+def send_debug(client: Client, chat: Chat, action: str, aid: int,
+               config_type: str = "", more: str = "") -> bool:
     # Send the debug message
+    result = False
+
     try:
+        if not chat:
+            return False
+
         text = get_debug_text(client, chat)
         text += (f"{lang('admin_group')}{lang('colon')}{code(aid)}\n"
                  f"{lang('action')}{lang('colon')}{code(action)}\n")
@@ -121,70 +135,33 @@ def send_debug(client: Client, chat: Chat, action: str, aid: int, config_type: s
         if more:
             text += f"{lang('more')}{lang('colon')}{code(more)}\n"
 
-        thread(send_message, (client, glovar.debug_channel_id, text))
-
-        return True
+        result = bool(send_message(client, glovar.debug_channel_id, text))
     except Exception as e:
         logger.warning(f"Send debug error: {e}", exc_info=True)
 
-    return False
+    return result
 
 
+@threaded()
 def share_data(client: Client, receivers: List[str], action: str, action_type: str,
                data: Union[bool, dict, int, str] = None, file: str = None, encrypt: bool = True) -> bool:
     # Use this function to share data in the channel
-    try:
-        thread(
-            target=share_data_thread,
-            args=(client, receivers, action, action_type, data, file, encrypt)
-        )
+    result = False
 
-        return True
-    except Exception as e:
-        logger.warning(f"Share data error: {e}", exc_info=True)
-
-    return False
-
-
-def share_data_thread(client: Client, receivers: List[str], action: str, action_type: str,
-                      data: Union[bool, dict, int, str] = None, file: str = None, encrypt: bool = True) -> bool:
-    # Share data thread
     try:
         if glovar.sender in receivers:
             receivers.remove(glovar.sender)
 
         if not receivers:
-            return True
+            return False
 
         if glovar.should_hide:
             channel_id = glovar.hide_channel_id
         else:
             channel_id = glovar.exchange_channel_id
 
-        if file:
-            text = format_data(
-                sender=glovar.sender,
-                receivers=receivers,
-                action=action,
-                action_type=action_type,
-                data=data
-            )
-
-            if encrypt:
-                # Encrypt the file, save to the tmp directory
-                file_path = get_new_path()
-                crypt_file("encrypt", file, file_path)
-            else:
-                # Send directly
-                file_path = file
-
-            result = send_document(client, channel_id, file_path, None, text)
-
-            # Delete the tmp file
-            if result:
-                for f in {file, file_path}:
-                    f.startswith("tmp/") and thread(delete_file, (f,))
-        else:
+        # Plain text
+        if not file:
             text = format_data(
                 sender=glovar.sender,
                 receivers=receivers,
@@ -193,31 +170,79 @@ def share_data_thread(client: Client, receivers: List[str], action: str, action_
                 data=data
             )
             result = send_message(client, channel_id, text)
+            return ((result is False and not glovar.should_hide)
+                    and share_data_failed(client, receivers, action, action_type, data, file, encrypt))
 
-        # Sending failed due to channel issue
-        if result is False and not glovar.should_hide:
-            # Use hide channel instead
-            exchange_to_hide(client)
-            thread(share_data, (client, receivers, action, action_type, data, file, encrypt))
+        # Share with a file
+        text = format_data(
+            sender=glovar.sender,
+            receivers=receivers,
+            action=action,
+            action_type=action_type,
+            data=data
+        )
 
-        return True
+        if encrypt:
+            # Encrypt the file, save to the tmp directory
+            file_path = get_new_path()
+            crypt_file("encrypt", file, file_path)
+        else:
+            # Send directly
+            file_path = file
+
+        result = send_document(client, channel_id, file_path, None, text)
+
+        if not result:
+            return ((result is False and not glovar.should_hide)
+                    and share_data_failed(client, receivers, action, action_type, data, file, encrypt))
+
+        # Delete the tmp file
+        for f in {file, file_path}:
+            f.startswith("tmp/") and thread(delete_file, (f,))
+
+        result = bool(result)
     except Exception as e:
-        logger.warning(f"Share data thread error: {e}", exc_info=True)
+        logger.warning(f"Share data error: {e}", exc_info=True)
 
-    return False
+    return result
+
+
+@threaded()
+def share_data_failed(client: Client, receivers: List[str], action: str, action_type: str,
+                      data: Union[bool, dict, int, str] = None, file: str = None, encrypt: bool = True) -> bool:
+    # Sharing data failed, use the exchange channel instead
+    result = False
+
+    try:
+        exchange_to_hide(client)
+        result = share_data(
+            client=client,
+            receivers=receivers,
+            action=action,
+            action_type=action_type,
+            data=data,
+            file=file,
+            encrypt=encrypt
+        )
+    except Exception as e:
+        logger.warning(f"Share data failed error: {e}", exc_info=True)
+
+    return result
 
 
 def share_regex_count(client: Client, word_type: str) -> bool:
     # Use this function to share regex count to REGEX
+    result = False
+
     try:
         if not glovar.regex.get(word_type):
-            return True
+            return False
 
         if not eval(f"glovar.{word_type}_words"):
-            return True
+            return False
 
         file = data_to_file(eval(f"glovar.{word_type}_words"))
-        share_data(
+        result = share_data(
             client=client,
             receivers=["REGEX"],
             action="regex",
@@ -225,9 +250,7 @@ def share_regex_count(client: Client, word_type: str) -> bool:
             data=f"{word_type}_words",
             file=file
         )
-
-        return True
     except Exception as e:
         logger.warning(f"Share regex update error: {e}", exc_info=True)
 
-    return False
+    return result
