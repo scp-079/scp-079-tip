dab89b98488c43b41060736d9784a62a655b6212
Xiao <xiao@scp-079.org>
2020-08-31 11:08:32 +0000

    ids.py

--- plugins/functions/ids.py
+++ plugins/functions/ids.py
@@ -28,11 +28,16 @@ logger = logging.getLogger(__name__)
 
 def init_group_id(gid: int) -> bool:
     # Init group data
+    result = False
+
     try:
         if gid == glovar.test_group_id:
             return False
 
         if gid in glovar.left_group_ids:
+            return False
+
+        if glovar.aio and any(gid == glovar.group_ids[group] for group in glovar.group_ids):
             return False
 
         if glovar.admin_ids.get(gid) is None:
@@ -55,34 +60,54 @@ def init_group_id(gid: int) -> bool:
             glovar.configs[gid] = deepcopy(glovar.default_config)
             save("configs")
 
+        if glovar.keywords.get(gid) is None:
+            glovar.keywords[gid] = deepcopy(glovar.default_keyword_data)
+            save("keywords")
+
+        if glovar.ots.get(gid) is None:
+            glovar.ots[gid] = deepcopy(glovar.default_ot_data)
+            save("ots")
+
+        if glovar.rms.get(gid) is None:
+            glovar.rms[gid] = deepcopy(glovar.default_rm_data)
+            save("rms")
+
+        if glovar.welcomes.get(gid) is None:
+            glovar.welcomes[gid] = deepcopy(glovar.default_welcome_data)
+            save("welcomes")
+
         if glovar.declared_message_ids.get(gid) is None:
             glovar.declared_message_ids[gid] = set()
-
-        if glovar.members.get(gid) is None:
-            glovar.members[gid] = {}
 
         if glovar.keyworded_ids.get(gid) is None:
             glovar.keyworded_ids[gid] = {}
 
+        if glovar.members.get(gid) is None:
+            glovar.members[gid] = {}
+
         if glovar.welcomed_ids.get(gid) is None:
             glovar.welcomed_ids[gid] = set()
 
-        return True
+        result = True
     except Exception as e:
         logger.warning(f"Init group id {gid} error: {e}", exc_info=True)
 
-    return False
+    return result
 
 
 def init_user_id(uid: int) -> bool:
     # Init user data
-    try:
-        if glovar.user_ids.get(uid) is None:
-            glovar.user_ids[uid] = deepcopy(glovar.default_user_status)
-            save("user_ids")
+    result = False
 
-        return True
+    try:
+        if glovar.user_ids.get(uid) is not None:
+            return True
+
+        glovar.user_ids[uid] = deepcopy(glovar.default_user_status)
+        save("user_ids")
+
+        result = True
     except Exception as e:
         logger.warning(f"Init user id {uid} error: {e}", exc_info=True)
 
-    return False
+    return result
