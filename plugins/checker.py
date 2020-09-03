d042cbcad5ec3b5b2df8df29d7e2beda28f57e1b
Xiao <xiao@scp-079.org>
2020-09-03 03:18:48 +0000

    0.2.1

--- plugins/checker.py
+++ plugins/checker.py
@@ -102,7 +102,7 @@ def check_custom(values: dict, broken: bool) -> str:
     for key in values:
         if values[key] in {"", "[DATA EXPUNGED]"}:
             result += f"[ERROR] [custom] {key} - please fill something except [DATA EXPUNGED]\n"
-        elif key.endswith("link") and (values[key].startswith("@") or " " in values[key]):
+        elif key.endswith("link") and (values[key].startswith("@") or " " in values[key] or "." not in values[key]):
             result += f"[ERROR] [custom] {key} - please input a valid url\n"
 
         if not broken or not result:
